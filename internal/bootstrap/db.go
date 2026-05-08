6573c994d24183d0cc0b9f221509dfe64818e4bf
GantangSatria <gantang610@gmail.com>
2026-05-08 09:51:45 +0700

    feat(db): implement retry logic for database connection and add keep-alive mechanism

--- internal/bootstrap/db.go
+++ internal/bootstrap/db.go
@@ -3,6 +3,7 @@ package bootstrap
 import (
 	"database/sql"
 	"log"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/GantangSatria/MyBank-BE/config"
@@ -23,6 +24,51 @@ func NewDB(cfg *config.Config) *sql.DB {
 	db.SetMaxOpenConns(25)
 	db.SetMaxIdleConns(10)
 
-	log.Println("[db] database connected successfully")
-	return db
+	// Retry ping database
+	maxRetries := 20
+
+	for i := 1; i <= maxRetries; i++ {
+		err := db.Ping()
+
+		if err == nil {
+			log.Println("[db] database connected successfully")
+
+			startKeepAlive(db)
+
+			return db
+		}
+
+		log.Printf(
+			"[db] waiting database connection (%d/%d): %v",
+			i,
+			maxRetries,
+			err,
+		)
+
+		time.Sleep(5 * time.Second)
+	}
+
+	log.Fatal("[db] failed to connect database after retries")
+
+	return nil
+}
+
+// helper free plan wkwk
+func startKeepAlive(db *sql.DB) {
+	ticker := time.NewTicker(30 * time.Minute)
+
+	go func() {
+		for range ticker.C {
+			var result int
+
+			err := db.QueryRow("SELECT 1").Scan(&result)
+
+			if err != nil {
+				log.Printf("[db] keepalive failed: %v", err)
+				continue
+			}
+
+			log.Println("[db] keepalive success")
+		}
+	}()
 }
