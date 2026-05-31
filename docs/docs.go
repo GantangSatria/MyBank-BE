927cef55ec02be0df6140211462d876a35be3fa1
GantangSatria <gantang610@gmail.com>
2026-06-01 03:05:47 +0700

    fix

--- docs/docs.go
+++ docs/docs.go
@@ -2071,7 +2071,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "mybank-be.fly.dev",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "MyBank API",
